e91aef13502d8f0cbea67f1d5db113b7f8a18880
Krupahombre <kacper.krupinski01@gmail.com>
2024-03-19 19:53:15 +0100

    update

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app.include_router(test_router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
