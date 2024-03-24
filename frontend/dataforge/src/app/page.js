27bb113473f99717b972669332fa4dd248d970d6
Dalentro <258953@student.pwr.edu.pl>
2024-03-24 20:56:52 +0100

    1.0 - init commit | front

---
+++ frontend/dataforge/src/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h1>Szalom,</h1>
+        <h2> this is demo of out home page,</h2>
+        <h3>innit?</h3>
+      </div>
+    </>
+  );
+}
