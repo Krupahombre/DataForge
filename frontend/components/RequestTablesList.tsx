9119624f97e458487a195a49334199042b4d5c35
Michał Malewicz <75280214+serwus701@users.noreply.github.com>
2024-05-17 12:09:30 +0200

    clear all tables fature

--- frontend/components/RequestTablesList.tsx
+++ frontend/components/RequestTablesList.tsx
@@ -1,6 +1,7 @@
 import styles from "../styles/Home.module.css";
 import React, { useState } from "react";
 import { IRequestTable } from "../common/models/IRequestTable";
+import { TrashIcon } from "@heroicons/react/24/solid";
 
 const RequestTablesList = (props) => {
   const tables: IRequestTable[] = props.requestTables;
@@ -8,9 +9,24 @@ const RequestTablesList = (props) => {
 
   // const handleClick = () => {};
 
+  const handleClear = () => {
+    localStorage.setItem("requestTables", JSON.stringify([]));
+    window.location.reload();
+  }
+
   return tables.length === 0 ? null : (
     <div className={styles.requestTablesListWrapper}>
       <h2>Request classes:</h2>
+      <div 
+        className={styles.displayResultTablesClearPanel} 
+        onClick={() => handleClear()}
+      >
+        {"Clear all tables "}
+        <TrashIcon
+              className={styles.recordXButton}
+            />
+      </div>
+      
       {tables.map((table) => (
         <div
           className={styles.requestTablesListItem}
